51c86be06ce066f30691bef13ee952c67947ca5a
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-11 20:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,12 @@ function switchOnCharmFromTip(){
 switch (tip) {
   case `generous`:
     return "Thank you so much.";
+    break;
   case `not as generous`:
     return `Thank you.`;
+    break;
   default:
     return `Bye.`;
+    break;
 }
 }
