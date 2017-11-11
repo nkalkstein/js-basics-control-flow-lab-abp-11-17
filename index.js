91e5d7cd56ed5c355224d2a75ba8c2ad6368ad95
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-11 19:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,40 @@ function ternaryCheckCity(city) {
         return "No go.";
 }
 }
+
+const charm = 'generous';
+ 
+let answer;
+
+
+switch (charm) {
+  case "generous":
+    answer = "Thank you so much.";
+    break;
+  case "not as generous":
+    answer = "Thank you.";
+    break;
+  default
+    answer = "Bye.";
+    break;
+}
+}
+
+const order = 'cheeseburger';
+ 
+let ingredients;
+ 
+switch (order) {
+  case 'cheeseburger':
+    ingredients = 'bun, burger, cheese, lettuce, tomato, onion';
+    break;
+  case 'hamburger':
+    ingredients = 'bun, burger, lettuce, tomato, onion';
+    break;
+  case 'malted':
+    ingredients = 'milk, ice cream, malted milk powder';
+    break;
+  default:
+    console.log("Sorry, that's not on the menu right now.");
+    break;
+}
